5fc2a1c3445c80bf1f356b089e457e9219ade97f
yehzu <yehzu2@gmail.com>
2024-04-13 17:16:27 +0800

    feat: consume fast pro xml from ftp

--- import_service_poc/import_shipment/apps.py
+++ import_service_poc/import_shipment/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ImportShipmentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'import_shipment'
+
+    def ready(self) -> None:
+        from import_shipment.views import import_fast_pro_ftp_schedule
+        import_fast_pro_ftp_schedule()
--- import_service_poc/import_shipment/views.py
+++ import_service_poc/import_shipment/views.py
@@ -1,8 +1,11 @@
+from background_task import background
 from import_shipment.factories.MblGatewayFactory import MblGatewayFactory
 from import_shipment.factories.PayloadInterpreterFactory import PayloadInterpreterFactory
 from import_shipment.factories.TradePartnerGatewayFactory import TradePartnerGatewayFactory
 from import_shipment.presenters.JsonPresenter import JsonPresenter
+from import_shipment.presenters.FakePresenter import FakePresenter
 from import_shipment.use_cases.UseCases import UseCases
+from import_shipment.protocols.FtpReader import FtpReader
 
 
 # Create your views here.
@@ -23,3 +26,25 @@ def import_fast_pro_xml_v1(request):
     usecase.import_mbl(tenant, mbl_payload, interpreter, mbl_repo, tp_repo, presenter)
 
     return presenter.get_view_model()
+
+
+def import_fast_pro_xml_ftp(payload):
+    tenant = "test tenant"  # or other way to get tenant, maybe in the header?
+    mbl_payload = payload
+
+    interpreter = PayloadInterpreterFactory.get("fast-pro-xml-v1")
+    mbl_repo = MblGatewayFactory.get()
+    tp_repo = TradePartnerGatewayFactory.get()
+
+    presenter = FakePresenter()
+
+    usecase = UseCases()
+    usecase.import_mbl(tenant, mbl_payload, interpreter, mbl_repo, tp_repo, presenter)
+
+    return presenter.get_view_model()
+
+
+@background(schedule=10)
+def import_fast_pro_ftp_schedule():
+    ftp = FtpReader()
+    ftp.get_file_content('path', import_fast_pro_xml_ftp)
